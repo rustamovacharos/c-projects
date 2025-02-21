2d5c954494fe2559b92e33842fa15e66d6c7f275
rustamovacharos <rustamovacharos5@gmail.com>
2025-02-21 15:43:27 +0500

    lab2

---
+++ main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+
+    //PROBLEM1
+    /* int a;
+     cin>>a;
+     if (a<0){
+       cout<<"negative";
+     } else if (a>0){
+       cout<<"Positive";
+     }else
+     {cout<<"zero";}
+   return 0;*/
+
+
+    //PROBLEM2
+    /*int a;
+      cin>>a;
+      if (a%2==0){
+        cout<<"even";
+      } else { cout<<"odd";
+             }*/
+
+    //PROBLEM 3
+    /* int a,b;
+     cin>>a >>b;
+     if (a>b) {
+       cout<<"a>b";}
+     else if (a<b){
+       cout<<"a<b";
+     }else {cout<<"a=b";
+           }
+   return 0;*/
+
+
+    //PROBLEM4 (nested if)
+    /* #include <iostream>
+     using namespace std;
+
+     int main() {
+         int a, b, c;
+         cout << "Enter three numbers: ";
+         cin >> a >> b >> c;
+
+         if (a > b) {
+             if (a > c) {
+                 cout << "a is the                     largest";
+             } else {
+                 cout << "c is the largest";
+             }
+         } else { // a <= b
+             if (b > c) {
+                 cout << "b is the largest";
+             } else if (c > b) {
+                 cout << "c is the largest";
+             } else { // if b == c and also a == b
+                 cout << "All numbers are equal";
+             }
+         }
+
+         return 0;
+     }
+   }
+
+
+ */
+
+
+    //PROBLEM5
+    /*int year;
+    cin>>year;
+    if (year%2==0) {
+      cout<<" a leap year";}
+    else {
+      cout<<" not leap year";
+    }
+  */
+
+    //PROBLEM6
+    /*int speed;
+    cin>>speed;
+    if (speed>=80){
+      cout<<"too fast";}
+    else if (speed<=20){
+      cout<<"too slow";}
+    else {cout<<"just right";}
+    return 0;*/
+
+    //PROBLEM7
+    /*int marks;
+  cin>>marks;
+  if (marks>=50){
+    cout<<"pass";
+  }else {cout<<"fail";
+  }
+    return 0;
+  }*/
+
+    //PROBLEM 8
+    /*int a;
+    cin>>a;
+    if (a>0){
+      if (a!=0){
+        cout<<"positive";}
+      else {
+        cout<<"zero";}}
+    else if (a<0) {if (a!=0){
+      cout<<"negative";}
+      else {cout<<"zero";}
+                  }
+    else
+    {cout<<"zero";}
+         }
+       */
